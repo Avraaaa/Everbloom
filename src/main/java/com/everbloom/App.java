4ee4f9b64f24165e6f72be8032625fcfa877f7e8
Avraaaa <arithro.ch12@gmail.com>
2026-09-07 01:01:47 +0600

    feat: add catalogue management UI

--- src/main/java/com/everbloom/App.java
+++ src/main/java/com/everbloom/App.java
@@ -1,5 +1,7 @@
 package com.everbloom;
 
+import com.everbloom.database.DatabaseConnection;
+import com.everbloom.database.DatabaseInitializer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,6 +10,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Objects;
+import java.sql.SQLException;
 
 public class App extends Application {
 
@@ -15,6 +18,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        initializeDatabase();
+
         FXMLLoader loader = new FXMLLoader(Objects.requireNonNull(
                 App.class.getResource("view/main-view.fxml"),
                 "Missing main view"
@@ -30,5 +35,14 @@ public class App extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private void initializeDatabase() throws IOException {
+        try {
+            DatabaseInitializer databaseInitializer = new DatabaseInitializer(new DatabaseConnection());
+            databaseInitializer.initialize();
+        } catch (SQLException exception) {
+            throw new IOException("Unable to initialize the application database.", exception);
+        }
     }
 }
